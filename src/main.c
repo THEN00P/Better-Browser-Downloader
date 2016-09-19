57598d8fe5afbc758e8bf4f595c5eec4a7106ea6
SMOKE5 <metallica5875@yahoo.com>
2016-09-18 21:47:38 -0400

    add license

--- src/main.c
+++ src/main.c
@@ -1,3 +1,21 @@
+/*
+	VPKMirror
+	Copyright (C) 2016, SMOKE
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "main.h"
 #include "init.h"
 #include "net.h"
@@ -78,7 +96,7 @@ int main() {
 	vpk_name = strchr(AppParam, '?')+1;
 
 	// create url based off the vpk name
-	char *vpk_url = malloc(1024 * sizeof(char));
+	char *vpk_url = malloc(512 * sizeof(char));
 	snprintf(vpk_url, 512, "http://vpkmirror.com/files/vpk/%s", vpk_name);
 
 	// download vpk
